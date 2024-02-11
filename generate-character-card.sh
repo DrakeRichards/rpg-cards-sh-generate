119147555a753e282f2931e7a3bc321fa193a3a9
Thomas Young <thomasjyoung8@gmail.com>
2024-02-11 02:18:27 -0600

    Updated submodules

--- generate-character-card.sh
+++ generate-character-card.sh
@@ -16,16 +16,16 @@ if [ ! -f "in/markdown/$1.md" ]; then
 fi
 
 # Pass the character's markdown file to the python script to convert it to yaml.
-python ./rpg-cards-py-obsidian-md-to-yaml/main.py "in/markdown/$1.md" "./out/yaml"
+python rpg-cards-py-obsidian-md-to-yaml/convert_character.py "in/markdown/$1.md" "./out/yaml"
 
 # Copy the character's yaml file to the Typst input location and overwrite the file there, if present.
-cp "./out/yaml/$1.yaml" "./src/typst/in/character.yaml"
+cp "out/yaml/$1.yaml" "rpg-cards-typst-templates/in/character.yaml"
 
 # Pull the name of the character's image file from their yaml file.
-image=$(grep "image:" "src/python/out/$1.yaml" | cut -d ' ' -f 2)
+image=$(grep "image:" "out/yaml/$1.yaml" | cut -d ' ' -f 2)
 
 # Copy the character's image to the Typst input location and overwrite the file there, if present.
-cp "in/images/$image" "src/typst/in/character.png"
+cp "in/images/$image" "rpg-cards-typst-templates/in/$image"
 
 # Run the character card generation script.
-typst compile src/typst/character.typ "out/$1.pdf"
+typst compile rpg-cards-typst-templates/src/cards/character/landscape.typ "out/pdf/$1.pdf" --root rpg-cards-typst-templates
